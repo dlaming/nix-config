6b4c3a9be54e1691b59d91e41500752867353cfc
dlaming <david@laming.me>
2026-09-04 08:26:54 +0000

    Fix sysupdate and sysupgrade alias

--- zsh.nix
+++ zsh.nix
@@ -20,8 +20,8 @@
       pay = "pueue add yt-dlp --no-playlist --cookies-from-browser chrome";
       gitacp = "git add -A && git commit -am. && git push";
       nixacp = "f() { git -C /etc/nixos add . && git -C /etc/nixos commit -m \"$1\" && git -C /etc/nixos push; }; f";
-      sysupdate = "nix flake update /etc/nixos";
-      sysupgrade = "nix flake update /etc/nixos && sudo nixos-rebuild switch --flake /etc/nixos#nixos";
+      sysupdate = "nix flake update --flake /etc/nixos";
+      sysupgrade = "nix flake update --flake /etc/nixos && sudo nixos-rebuild switch --flake /etc/nixos#nixos";
       sysrebuild = "sudo nixos-rebuild switch --flake /etc/nixos#nixos";
       systest = "sudo nixos-rebuild test --flake /etc/nixos#nixos";
     };
